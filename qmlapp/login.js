2c6974a1a6c6d8781f22b01f84f20ae75ff3bfe2
Cody Scott <cody.j.b.scott@gmail.com>
2016-09-16 17:46:31 -0400

    Updating PyQt5 to 5.7 which has the QML files in the wheel

--- qmlapp/login.js
+++ qmlapp/login.js
@@ -40,7 +40,7 @@ function onLoad(){
     // Populate Last Server
     loginManager.lastServer.connect(function(lastServer){
         if (lastServer != ""){
-            console.log(lastServer);
+            //console.log(lastServer);
             cmbServers.currentIndex = cmbServers.find(lastServer);
         }
     });
